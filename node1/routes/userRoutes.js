231d4496aea6ef6d29fbc387176456a4193be668
joseph skokan <joeskokan20@gmail.com>
2025-03-06 16:34:52 -0500

    route fix

--- node1/routes/userRoutes.js
+++ node1/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/userController");
+const { everyUser } = require("../controllers/userController");
 
-router.get("/users", userController.everyUser);
+router.get("/users", everyUser);
 
 module.exports = router;
